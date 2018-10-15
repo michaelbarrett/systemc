d7619fb08a6d8de2999aae097288a561d889f331
MichaelBarrett219 <michaelbarrett000@gmail.com>
2018-10-15 15:40:50 -0400

    added method declarations for controller

--- cpu/controller.h
+++ cpu/controller.h
@@ -3,10 +3,24 @@
 #include "systemc.h"
 
 SC_MODULE(controller) {
+  sc_in <bool> clock;
   sc_in <bool> z_flag;
   sc_in <bool> n_flag;
   sc_in <bool> c_flag;
   sc_in <bool> l_flag;
 
-  SC_HAS_PROCESS(prc_pipeline);
+  SC_HAS_PROCESS(controller);
+
+ controller(sc_module_name name) : sc_module(name) {
+    SC_METHOD(prc_if);
+    sensitive << clock.pos();
+    SC_METHOD(prc_rd);
+    sensitive << clock.pos();
+    SC_METHOD(prc_exe);
+    sensitive << clock.pos();
+    SC_METHOD(prc_mem);
+    sensitive << clock.pos();
+    SC_METHOD(prc_wb);
+    sensitive << clock.neg();
+  }
 }
