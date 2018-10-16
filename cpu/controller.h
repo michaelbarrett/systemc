422f99153cd6d0c0c36b858a43504a69cda23ba5
MichaelBarrett219 <michaelbarrett000@gmail.com>
2018-10-16 14:44:48 -0400

    updated controller

--- cpu/controller.h
+++ cpu/controller.h
@@ -13,7 +13,7 @@ SC_MODULE(controller) {
   sc_in <int> instr_from_pm;
   sc_out <int> addr_to_pm;
 
-  //to and from ALU (control signals)
+  //to ALU (control signals)
   sc_out <bool> with_twos_to_alu;
   sc_out <bool> use_imm_to_alu;
   sc_out <bool> set_oup_reg_to_alu;
@@ -26,9 +26,27 @@ SC_MODULE(controller) {
   sc_out <bool> ash_instr_to_alu;
   sc_out <bool> en_to_alu;
 
+  //to and from ALU (data)
+  sc_in <int> result_from_alu;
+  sc_out <int> rd_to_alu;
+  sc_out <int> rs_to_alu;
+  sc_out <int> imm_to_alu;
+
+  //to and from DM
+  sc_in <int> data_from_dm;
+  sc_out <int> addr_to_dm;
+  sc_out <int> d_in_to_dm;
+
+  //to and from RF
+  sc_in <int> data_from_rf;
+  sc_out <int> rs_to_rf;
+  sc_out <int> rd_to_rf;
+
   int ir = 0; //instruction register (init = 0)
   int pc = 0; //program counter (init = 0)
 
+  int mem_result; //result of memory op
+  
   SC_HAS_PROCESS(controller);
 
  controller(sc_module_name name) : sc_module(name) {
