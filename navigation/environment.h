a2bdc326d524f5f641c754f43cd5bd4787339578
MichaelBarrett219 <michaelbarrett000@gmail.com>
2018-10-24 18:10:34 -0400

    started proj 2 template

---
+++ navigation/environment.h
@@ -0,0 +1,8 @@
+//environment.h
+#include "systemc.h"
+
+SC_MODULE(environment) {
+  //start gps at 0,0
+  int robotx = 0;
+  int roboty = 0;
+}
---
+++ navigation/robot.h
@@ -0,0 +1,5 @@
+//robot.h
+#inlcude "systemc.h"
+SC_MODULE (robot) {
+  
+}
